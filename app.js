5960138798ec8dba7e68dd154b4401833daa5cb1
venux404 <venux404@gmail.com>
2020-06-11 14:17:53 +0700

    initial git

---
+++ app.js
@@ -0,0 +1,5 @@
+import './src/styles/style.css';
+import './src/script/component/app-bar.js';
+import './src/script/component/search-bar.js';
+import main from './src/script/view/main.js';
+document.addEventListener("DOMContentLoaded", main);
